c8a41d72afe37a220e9d75872d65b133f7648894
unknown <denis.flank@gmail.com>
2025-04-12 19:00:08 +0200

    created new project, configured MySQL

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/my_page', function () {
+    return 'this is my page';
+});
