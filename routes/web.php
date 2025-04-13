572467c92811f070f23aa2a591b8211c30742490
unknown <denis.flank@gmail.com>
2025-04-13 09:44:48 +0200

    add posts route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ContactsController;
 use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\PostsController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\FeaturesController;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +13,8 @@ Route::get('/', [HomePageController::class,'index']);
 Route::get('/about', [AboutController::class, 'index']);
 
 Route::get('/products', [ProductsController::class, 'index']);
+
+Route::get('/posts', [PostsController::class, 'index']);
 
 Route::get('/features', [FeaturesController::class, 'index']);
 
