dff8458aa882305608789a4d130765190d68404e
unknown <denis.flank@gmail.com>
2025-04-13 09:19:53 +0200

    add 5 routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\ContactsController;
+use App\Http\Controllers\HomePageController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\FeaturesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomePageController::class,'index']);
+
+Route::get('/about', [AboutController::class, 'index']);
+
+Route::get('/products', [ProductsController::class, 'index']);
+
+Route::get('/features', [FeaturesController::class, 'index']);
+
+Route::get('/contacts', [ContactsController::class, 'index']);
