ae5f3abe3203540d6a124626d001d15b8261729a
@IvanMakarishchev <denis.flank@gmail.com>
2025-04-14 09:12:14 +0200

    connect bootstrap and vite, try base styles

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,22 +5,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{ route('main.index') }}">Main</a></li>
-                <li><a href="{{ route('about.index') }}">About</a></li>
-                <li><a href="{{ route('product.index') }}">Products</a></li>
-                <li><a href="{{ route('post.index') }}">Posts</a></li>
-                <li><a href="{{ route('feature.index') }}">Features</a></li>
-                <li><a href="{{ route('contact.index') }}">Contacts</a></li>
-            </ul>
-        </nav>
-    </div>
-    @yield('content')
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="{{ route('main.index') }}">Main</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('about.index') }}">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('product.index') }}">Products</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('feature.index') }}">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            @yield('content')
+        </div>
 </body>
 
 </html>
